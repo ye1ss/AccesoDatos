90a963daa59080b7357fd190b3b9a407baa10f41
yeismil <123468357+yeismil@users.noreply.github.com>
2023-10-02 20:58:44 +0200

    no se uy bn q es esto

--- Actividad5/src/main/java/com/mycompany/actividad5/Actividad5.java
+++ Actividad5/src/main/java/com/mycompany/actividad5/Actividad5.java
@@ -13,7 +13,11 @@ import javax.swing.JPanel;
  * @author Alumno
  */
 public class Actividad5 {
-    /**/
+    /*En este ejercicio vamos a manejar con Oracle Java  la clase file para archivos y directorios.
+    1.- Escribir un programa que muestre los nombres de los archivos y directorios que se encuentren en el directorio que se le pida al usuario en el programa –
+    2.- Escribir un programa que muestre los nombres de los archivos de un directorio, que se obtendrá del usuario cuya extensión coincida con la que se le pedirá al usuario.
+    En este programa utilice JOption Pane para pedir los datos al usuario y cree un menú para seleccionar las opciones 
+    1.Directorio y realice el apartado 1 y 2.Archivos y realice el apartado 2*/
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         
