7e43a5a90a60a3232823219c8a4c4261b51f2929
yeismil <136033070+ye1ss@users.noreply.github.com>
2023-10-11 11:09:44 +0200

    ej6 empezando

---
+++ Actividad6/src/main/java/com/mycompany/actividad6/Actividad6.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.actividad6;
+
+import java.io.File;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Actividad6 {
+
+    public static void main(String[] args) {
+        int opciones= Integer.parseInt(JOptionPane.showInputDialog("1.Pegar ruta\n2.Copiar archivo"));
+        
+        switch(opciones){
+            case 1:
+                
+                break;
+            case 2:
+                
+                break;
+            default:
+                
+        }
+    }
+}
