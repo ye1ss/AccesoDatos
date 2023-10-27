402507d4ad40dbc2353b787a14ac27dda2201e3e
ye1ss <yeismil18@gmail.com>
2023-10-27 17:41:20 +0200

    actividad final empezando

---
+++ ActividadFinal/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import javax.swing.*;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        JFrame ventana= new JFrame("FIle manager");
+    }
+}
