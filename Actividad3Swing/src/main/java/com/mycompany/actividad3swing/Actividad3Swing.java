5e16a10ce4680337e1c1408e2df0aec620e3564f
yeismil <136033070+ye1ss@users.noreply.github.com>
2023-10-28 18:34:32 +0200

    activiadad 1 terminada

--- Actividad3Swing/src/main/java/com/mycompany/actividad3swing/Actividad3Swing.java
+++ Actividad3Swing/src/main/java/com/mycompany/actividad3swing/Actividad3Swing.java
@@ -4,10 +4,13 @@
 
 package com.mycompany.actividad3swing;
 
-import java.awt.FlowLayout;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
 
 /**
  *
@@ -16,13 +19,34 @@ import javax.swing.JPanel;
 public class Actividad3Swing {
 
     public static void main(String[] args) {
-        JFrame ventana= new JFrame("Crear fichero");
-        
-        JPanel panel= new JPanel();
-        panel.setLayout(new FlowLayout()); //Esto establece los compoentes del panel de izquierda da derecha
-        
-        JLabel lab1= new JLabel("Escribe una ruta donde inttroducir el fichero: ");
-        
-        
+        JFrame ventana = new JFrame("Ficheros");
+
+        JFileChooser selectorArchivo = new JFileChooser();
+        selectorArchivo.setFileSelectionMode(JFileChooser.FILES_ONLY);
+
+        int resultado = selectorArchivo.showOpenDialog(ventana);
+
+        if (resultado == JFileChooser.APPROVE_OPTION) {
+            File archivo = selectorArchivo.getSelectedFile();
+
+            try {
+                FileReader lector = new FileReader(archivo);
+                JTextArea areaTexto = new JTextArea();
+
+                int caracter;
+                while ((caracter = lector.read()) != -1) {
+                    areaTexto.append(Character.toString((char) caracter));
+                }
+
+                ventana.add(areaTexto);
+                ventana.setSize(300, 200);
+                ventana.setVisible(true);
+
+                lector.close();
+            } catch (IOException e) {
+                System.out.println("Hubo un error al leer el archivo");
+                e.printStackTrace();
+            }
+        }
     }
 }
