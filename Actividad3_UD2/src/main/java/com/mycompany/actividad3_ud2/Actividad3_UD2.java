80811216cd29fb57c19ec1dcb1e6550f9880b3d7
yeismil <136033070+ye1ss@users.noreply.github.com>
2023-11-14 07:28:04 +0100

    creando Actividad3

---
+++ Actividad3_UD2/src/main/java/com/mycompany/actividad3_ud2/Actividad3_UD2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.actividad3_ud2;
+
+/**
+ *
+ * @author Yeismil P
+ */
+public class Actividad3_UD2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
